4f9f23c1dd521371e1e9a7f2da215132cbe9a8fe
Mohammad Askar <m.askar@isecur1ty.org>
2016-10-21 17:20:37 +0300

    Update transposition-cipher.py

--- transposition-cipher.py
+++ transposition-cipher.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 
-plain_text1 = "My Secret !:D"
+plain_text1 = "abcdxxxxx"
 plain_text = list(plain_text1)
-key = [(0,3),(1,6), (4,5), (7,9)]
+key = [(0,3),(1,2), (4,5), (7,9),(5,9),(10,25)]
 counter = 0
-def encryption_algo(plain_text1):
+def encryption_algo(ptx):
  for k in key:
-  temp = plain_text[k[0]]
-  plain_text[k[0]] = plain_text[k[1]]
-  plain_text[k[1]] = temp
- return "".join(plain_text)
+  try:
+   temp = ptx[k[0]]
+   ptx[k[0]] = ptx[k[1]]
+   ptx[k[1]] = temp
+  except:
+	  pass
+ return "".join(ptx)
 
 #print encryption_algo(plain_text1)
 
 def decryption_algo(plain_text1):
  for k in key:
-  temp = plain_text[k[1]]
-  plain_text[k[1]] = plain_text[k[0]]
-  plain_text[k[0]] = temp
-
+  try:
+   temp = plain_text[k[1]]
+   plain_text[k[1]] = plain_text[k[0]]
+   plain_text[k[0]] = temp
+  except:
+	  pass
  return "".join(plain_text)
 
 cipher_text = encryption_algo(plain_text)
