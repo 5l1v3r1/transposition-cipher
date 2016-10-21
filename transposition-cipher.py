2afe740ac086c9b51a3e91d2eec602aaf1b7e399
Mohammad Askar <m.askar@isecur1ty.org>
2016-10-21 16:54:46 +0300

    Update transposition-cipher.py

--- transposition-cipher.py
+++ transposition-cipher.py
@@ -6,13 +6,13 @@ key = [(0,3),(1,2)]
 counter = 0
 for k in key:
   temp = plain_text[k[0]]
-  #print "temp now is equal {0}".format(temp)
+  
 
   plain_text[k[0]] = plain_text[k[1]]
-  #print "plain_text[k[1]] which is {0} equals plain_text[k[0]] which is {1}".format(plain_text)
+ 
 
   plain_text[k[1]] = temp
 
-  #print "plain_text[k[1]] which is {0} equals temp which is {1}".format(plain_text[k[1]],temp)
+ 
 
 print "".join(plain_text)
