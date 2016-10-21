5d3ecbf1d80323322485760e6441c65fecdc9452
Mohammad Askar <m.askar@isecur1ty.org>
2016-10-21 17:00:02 +0300

    Update transposition-cipher.py

--- transposition-cipher.py
+++ transposition-cipher.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python
 
-plain_text1 = "Mohammad Askar"
+plain_text1 = "123456"
 plain_text = list(plain_text1)
-key = [(0,3),(1,2)]
+key = [(0,3),(1,2), (4,5)]
 counter = 0
+def encryption_algo(plain_text1):
 for k in key:
   temp = plain_text[k[0]]
-  
-
   plain_text[k[0]] = plain_text[k[1]]
- 
-
   plain_text[k[1]] = temp
 
- 
-
-print "".join(plain_text)
+return "".join(plain_text)
