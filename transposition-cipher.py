c195317d2b412e0cecf4c7c621097324f9563747
Mohammad Askar <m.askar@isecur1ty.org>
2016-10-21 17:13:21 +0300

    Update transposition-cipher.py

--- transposition-cipher.py
+++ transposition-cipher.py
@@ -11,4 +11,21 @@ def encryption_algo(plain_text1):
   plain_text[k[1]] = temp
  return "".join(plain_text)
 
-print encryption_algo(plain_text1)
+#print encryption_algo(plain_text1)
+
+def decryption_algo(plain_text1):
+ for k in key:
+  temp = plain_text[k[1]]
+  plain_text[k[1]] = plain_text[k[0]]
+  plain_text[k[0]] = temp
+
+ return "".join(plain_text)
+
+cipher_text = encryption_algo(plain_text)
+decrypted_text = decryption_algo(cipher_text)
+print "Your Plain Text is : {0}".format(plain_text1)
+print "*" * 25
+print "Your Encrypted Text is : {0} ".format(cipher_text)
+print "*" * 25
+print "Your Decrypted Text is : {0}".format(decrypted_text)
+
